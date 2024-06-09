e42b49980f93fe757e3341934a38a233da5fed02
Sindeak or kimjinh1111 <161195769+jinheyopp123@users.noreply.github.com>
2024-06-09 18:21:15 +0900

    app.js 모듈화 작업

--- charts.js
+++ charts.js
@@ -1,1 +1,8 @@
+// charts.js
 
+function initializeCharts() {
+    // Example: Initialize chart.js or any other chart libraries
+    console.log('Charts initialized.');
+}
+
+export { initializeCharts };
